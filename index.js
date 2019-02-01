dac6f496fee75ce93bd78148d48c815e256724c5
Shahad Bakoos <shahadbakoos@gmail.com>
2019-02-01 15:23:44 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+
 // Code your solution in this file
+function findMatching (drivers, y) {
+  return drivers.filter(function (x) {
+    return x.toLowerCase() == y.toLowerCase();
+  });
+}
+function fuzzyMatch (arr, partofName) {
+  let fullName = partofName.length;
+  return arr.filter(function (x) {
+    return x.slice(0, fullName) === partofName;
+  });
+}
+function matchName (drivers, y) {
+  return drivers.filter(function (x) {
+    return x.name.toLowerCase() == y.toLowerCase();
+  });
+}
